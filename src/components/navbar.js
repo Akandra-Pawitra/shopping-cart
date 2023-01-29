6b2a457880ab73032a31d2592e18039313b5a167
Akandra-Pawitra <akandrapawitra@gmail.com>
2023-01-29 18:10:10 +0700

    Add pages routing

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Navbar () {
   return (
     <div id='navbar'>
-      <p id='nav-home'>HOME</p>
-      <p id='nav-store'>STORE</p>
+      <a id='nav-home'>
+        <Link to={'/'}>HOME</Link>
+      </a>
+      <a id='nav-store'>
+        <Link to={'/store'}>STORE</Link>
+      </a>
     </div>
   )
 }
