a64dfe4c837bdf11c3077a89158ea029ff8f7d0d
Akandra-Pawitra <akandrapawitra@gmail.com>
2023-01-31 17:19:43 +0700

    Add homepage content

--- src/components/home.js
+++ src/components/home.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Home () {
   return (
-    <div id='home'>Home</div>
+    <div id='home'>
+      <div id='welcome-text'>WELCOME</div>
+      <div id='welcome-img'>Place Image here</div>
+    </div>
   )
 }
 
